50191b97f51d36c0bad9ce3036b27c2f6ca3a52e
Cezary Buliszak <cezary.buliszak@gmail.com>
2018-02-02 11:58:47 +0000

    Silenced some tslint errors.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+/* tslint:disable import-name */
 import Vue from 'vue';
 import App from './components/App.vue';
 import './scss/app.scss';
 
 new Vue({
   el: '#js--app',
-  render (createElement) {
+  render(createElement) {
     return createElement(App);
   },
 });
--- src/vue-shims.d.ts
+++ src/vue-shims.d.ts
@@ -1,4 +1,5 @@
-declare module "*.vue" {
-  import Vue from "vue";
+/* tslint:disable import-name */
+declare module '*.vue' {
+  import Vue from 'vue';
   export default Vue;
 }
